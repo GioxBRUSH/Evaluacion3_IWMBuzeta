cfb27a2fc270c5eb32960665ecbb9cd63c358c64
GioxBRUSH <105091966+GioxBRUSH@users.noreply.github.com>
2023-12-13 15:54:51 -0300

    feat: add button delete

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # Evaluacion3_IWM
+# JSON Server Example
+
+Este es un ejemplo de cómo usar JSON Server para simular una API REST con datos JSON. En este caso, hemos creado un conjunto de datos simulados con posts, comentarios y un perfil.
+
+## Requisitos previos
+
+Asegúrate de tener [Node.js](https://nodejs.org/) instalado en tu máquina.
+
+## Instalación
+
+1. Clona este repositorio o descarga el código.
+
+    ```bash
+    git clone https://github.com/GioxBRUSH/Evaluacion3_IWMBuzeta
+    ```
+
+2. Navega al directorio del proyecto.
+
+    ```bash
+    cd backend
+    ```
+
+3. Instala las dependencias.
+
+    ```bash
+    npm install
+    ```
+
+## Uso
+
+1. Inicia JSON Server.
+
+    ```bash
+    json-server --watch posts.json
+    ```
+
+2. JSON Server se ejecutará en `http://localhost:3000`.
+
+# Frontend
+## Prerequisitos
+ - [Node](https://nodejs.org/en)
+
+Navegue hasta el directorio del proyecto
+
+```bash
+  cd frontend
+```
+
+Installe todas las dependencias
+
+```bash
+  npm install
+```
+
+Finalmente, ejecute el proyecto 
+
+```bash
+  npx expo start --web
+```
--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,118 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { ListItem, Avatar } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Image } from 'react-native';
+import { Modal, Pressable } from 'react-native';
 
 export default function App() {
 
-  const [products]
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedPostId, setSelectedPostId] = useState(null); 
+
+  const getPosts = async() => {
+    try {
+        const response = await axios.get('http://localhost:3000/posts')
+        setPosts(response.data);
+        setComments(response.data.comments);
+    } catch (error) {
+        console.error(error);
+    }
+ }
+
+
+
+  const deletePost = async(id) => {
+    
+    try {
+      await axios.delete(`http://localhost:3000/posts/${id}`);
+      getPosts();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+      getPosts();
+  }, []);
+
+
+
   return (
+
+    <ScrollView>
+      
+      {posts.map((post) => {
+        return (
+          <ListItem
+            key={post.id}
+            bottomDivider
+            
+          >
+            <ListItem.Content>
+              <ListItem.Title>{post.title}</ListItem.Title>
+              <ListItem.Subtitle>{post.author}</ListItem.Subtitle>
+              <Image source={{ uri: post.image }} style={{ width: 300, height: 300 }} />
+
+
+              <View style={{ flexDirection: 'row',
+            gap:3, marginTop:3}}>
+
+              <Button
+                  title="Borrar"
+                  onPress={() => {
+                    setModalVisible(true),
+                    setSelectedPostId(post.id);
+                  }}
+                  color="#E37399"
+                />
+              </View>
+            </ListItem.Content>
+          </ListItem>
+        );
+      })}
+
+<Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>¿Estás seguro de que quieres borrar este post?</Text>
+            <View style ={{flexDirection: 'row', gap:4 }}>
+
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                deletePost(selectedPostId);
+              }}
+            >
+              <Text style={styles.textStyle}>Sí</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>No</Text>
+            </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      
+
+    </ScrollView>
    
   );
 }
@@ -16,4 +124,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
